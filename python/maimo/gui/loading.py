e66663aeae897a368478f28c81d03f729e39eb1c
gentooza <joa.cuellar@riseup.net>
2013-12-22 17:01:00 +0100

    initial python approach

---
+++ python/maimo/gui/loading.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+"""@package main
+ Documentation for this module.
+ 
+ More details.
+"""
+from gi.repository import Gtk
+
+
+
+
+class DlgLoading:
+     """Loading dialog"""
+     def __init__(self):
+         self.builder = Gtk.Builder()
+         self.builder.add_from_file("./gui/Loading.glade")
+         self.builder.add_objects_from_file("./gui/Loading.glade", ("progressbar", "butcancel"))
+         self.cancel  = self.builder.get_object("butcancel")
+         self.cancel.connect("clicked", self.onButtonCancel)
+         
+
+         self.window = self.builder.get_object("DlgLoading1")       
+
+     def show(self):
+         self.window.show_all()
+
+     def onButtonCancel(self):
+        print "estamos en al funcion, capullo!"
+        self.destroy()   
+
+
+
---
+++ python/maimo/maimo.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+"""@package main
+ Documentation for this module.
+ 
+ More details.
+"""
+
+
+from gi.repository import Gtk
+import gui.loading
+
+
+
+class Maimoglade:
+	def onDeleteWindow(self, *args):
+		Gtk.main_quit(*args)
+
+	def onButtonPressed(self, button2):
+		dialog = gui.loading.DlgLoading()
+		dialog.run()
+        	print("Hello World")
+		
+
+store = Gtk.ListStore(str)
+store.append(['hello'])
+store.append(['goodbye'])
+
+builder = Gtk.Builder()
+builder.add_from_file("Maimo.glade")
+builder.connect_signals(Maimoglade())
+
+
+combo = builder.get_object("combobox1")
+combo.set_model(store)
+#combo.append_text("store")
+#combo.append("store1")
+#combo.append("store1")
+
+render = Gtk.CellRendererText()
+combo.pack_start(render, True)
+combo.add_attribute(render, 'text', 0)
+
+
+window = builder.get_object("window1")
+
+window.show_all()
+
+Gtk.main()
+#print store
+
+
+
+
+
+
+
